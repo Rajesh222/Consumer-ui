8bcaef2d917cb368b2061710fffa1576fb409529
Satyam Kumar <satyamk328@gmail.com>
2019-01-24 17:04:14 +0530

    added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import Resetpassword from './components/Resetpassword';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 import Home from './components/Home';
+import About from './components/common/About';
+import Privacy from './components/common/Privacy';
+import Terms from './components/common/Terms';
+import Contact from './components/common/Contact';
 
 class App extends Component {
   render() {
@@ -14,6 +18,10 @@ class App extends Component {
      <Router>
        <div>
          <Route path="/" component={Home} />
+         <Route path="/about" component={About} />
+         <Route path="/privacy" component={Privacy} />
+         <Route path="/terms" component={Terms} />
+         <Route path="/contact" component={Contact} />
          <Route path="/login" component={Login} />
          <Route path="/dashboard" component={Dashboard} />
          <Route path="/forgotpassword" component={Forgotpassword} />
