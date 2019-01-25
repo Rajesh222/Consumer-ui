7c7574494f11854d29659dc4c4cae9b058da8a22
Satyam Kumar <satyamk328@gmail.com>
2019-01-25 17:29:41 +0530

    added

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Header from '../common/Header';
 import Footer from '../common/Footer';
-import Dashboard from '../Dashboard';
 
 class Home extends React.Component {
  
@@ -10,7 +9,58 @@ class Home extends React.Component {
     return (
       <div>
         <Header />
-        <div>Hello</div>
+        <section className="service-inner">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-md-offset-3">
+                        <div class="title">
+                            <h2>Our Service</h2>
+                            <p>We Provide Taxi Services &amp; Tours Packages in Delhi-NCR and Nearby Cities !</p>
+                        </div>
+                     </div>
+                </div>
+                <div className="row">
+                     <div className="col-sm-12">
+                          <div className="col-sm-3 inner-box">
+                               <article>
+                                    <div className="icon"><span className="flaticon-placeholder"></span></div>
+                                    <div className="content-text">
+                                        <h5>Top Tours Packages</h5>
+                                        <p>We provide best tours and taxi services .</p>
+                                    </div>
+                               </article>
+                          </div>
+                          <div className="col-sm-3 inner-box">
+                              <article>
+                                  <div className="icon"><span className="flaticon-placeholder"></span></div>
+                                  <div className="content-text">
+                                      <h5>Top Tours Packages</h5>
+                                      <p>We provide best tours and taxi services .</p>
+                                  </div>
+                              </article>
+                          </div>
+                          <div className="col-sm-3 inner-box">
+                              <article>
+                                  <div className="icon"><span className="flaticon-placeholder"></span></div>
+                                  <div className="content-text">
+                                      <h5>Top Tours Packages</h5>
+                                      <p>We provide best tours and taxi services .</p>
+                                  </div>
+                              </article>
+                          </div>
+                          <div className="col-sm-3 inner-box">
+                              <article>
+                                  <div className="icon"><span className="flaticon-placeholder"></span></div>
+                                  <div className="content-text">
+                                      <h5>Top Tours Packages</h5>
+                                      <p>We provide best tours and taxi services .</p>
+                                  </div>
+                              </article>
+                          </div>
+                     </div>
+                </div>
+            </div>
+        </section>
         <Footer />
       </div>     
     );
