982d5077358b3d7b56281643b1f9592a910e6ee8
Satyam Kumar <satyamk328@gmail.com>
2019-01-25 10:58:32 +0530

    added

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -3,8 +3,7 @@ import PropTypes from 'prop-types';
 import "./index.scss";
 import config from '../../../config';
 import Axios from 'axios';
-import Footer from '../Footer'
-import { Navbar, Nav, NavItem, NavDropdown, MenuItem, Glyphicon } from 'react-bootstrap';
+import { Navbar, Nav, NavItem, NavDropdown,MenuItem, Glyphicon } from 'react-bootstrap';
 import logo from '../../../img/logo.png';
 
 
@@ -29,44 +28,58 @@ class Header extends React.Component {
   render() {
     const loginData = localStorage.getItem('loginData') && JSON.parse(localStorage.getItem('loginData')) ;
     return (
-       <Navbar>
-           <Navbar.Header>
-             <Navbar.Brand>
-                <a href="/">
-                   <img src={logo} className="img-resposive" />
-                </a>                  
-              </Navbar.Brand>
-              <Navbar.Toggle data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" data-target="#navbar" aria-expanded="false" aria-controls="navbar"/>
-            </Navbar.Header>
-            <Navbar.Collapse>
-              <Nav>
-                <NavItem className="active" eventKey={1} href="#"> Home </NavItem>
-                <NavItem className="active" eventKey={2} href="#">About Us</NavItem>
-                <NavItem  className="active" eventKey={3} href="#">Bus Booking</NavItem>
-                <NavItem  className="active" eventKey={4} href="#"> Taxi Service </NavItem>
-                <NavItem className="active" eventKey={5} href="#"> FAQs </NavItem>  
-                <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-                    <MenuItem eventKey={3.1}>Action</MenuItem>
-                    <MenuItem eventKey={3.2}>Another action</MenuItem>
-                    <MenuItem eventKey={3.3}>Something else here</MenuItem>
-                    <MenuItem eventKey={3.3}>Separated link</MenuItem>
-                </NavDropdown>
-                <NavItem className="active" eventKey={5} href="#"> Contact US </NavItem>  
-              </Nav>
-            </Navbar.Collapse>
-          { !loginData ?
-            <div className="collapse navbar-collapse sign-in" id="navbarNavDropdown">
-            <ul className="navbar-nav">
-              <button><a href="/signup"><span className="glyphicon glyphicon-user"></span> Sign Up</a></button>
-              <button><a href="/login"><span className="glyphicon glyphicon-log-in"></span> Login</a></button>
-            </ul>
-          </div>: 
-          <div className="sign-in">
-            <button type= 'button' onClick={(e)=>{this.handleLogOut(e)}}>Log Out</button>
-          </div>
-          }
-          </Navbar>
-      
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div class="container">
+            <div class="navbar-header">
+                <a className="navbar-brand" href="/"><img src={logo} className="img-resposive" /></a> 
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav">
+                  <li className="active"><a className="nav-link" href="/">Home</a></li>
+                  <li className=""><a className="nav-link" href="/">About</a></li>
+                  <li className=""><a className="nav-link" href="/">Bus Booking</a></li>
+                  <li className="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                    <ul className="dropdown-menu">
+                      <li><a href="#">Action</a></li>
+                      <li><a href="#">Another action</a></li>
+                      <li><a href="#">Something else here</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">Separated link</a></li>
+                      <li role="separator" class="divider"></li>
+                      <li><a href="#">One more separated link</a></li>
+                    </ul>
+                  </li>                  
+                  <li className="dropdown">
+                      <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true">Taxi Services<span class="caret"></span></a>
+                      <ul className="dropdown-menu">
+                        <li><a href="economy-car-rental.html">Economy Car Rental</a></li>
+                        <li><a href="suvmuv-cars.html">SUV/MUV Car Rental</a></li>
+                        <li><a href="coach-rental.html">Coach Rental</a></li>
+                      </ul>
+                  </li>
+                  <li className="active"><a href="contact.html">Contact Us</a></li>
+                </ul>
+                { !loginData ?
+                  <div className="collapse navbar-collapse sign-in" id="navbarNavDropdown">
+                  <ul className="navbar-nav">
+                    <button><a href="/signup"><span className="glyphicon glyphicon-user"></span> Sign Up</a></button>
+                    <button><a href="/login"><span className="glyphicon glyphicon-log-in"></span> Login</a></button>
+                  </ul>
+                </div>: 
+                <div className="sign-in">
+                  <button type= 'button' onClick={(e)=>{this.handleLogOut(e)}}>Log Out</button>
+                </div>
+                }
+            </div>
+        </div>
+      </nav>   
     );
   }
 }
