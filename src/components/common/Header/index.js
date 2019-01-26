37d5d9fd1a34819e3564ffde232272eea9d25103
Satyam Kumar <satyamk328@gmail.com>
2019-01-26 23:32:42 +0530

    added

--- src/components/common/Header/index.js
+++ src/components/common/Header/index.js
@@ -3,7 +3,8 @@ import PropTypes from 'prop-types';
 import "./index.scss";
 import Axios from 'axios';
 import config from '../../../config';
-import {  Navbar, Nav, NavItem, NavDropdown,MenuItem, Glyphicon } from 'react-bootstrap';
+import {  Navbar, Nav, NavItem, NavDropdown,MenuItem, NavbarBrand,Glyphicon } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import logo from '../../../img/logo.png';
 
 class Header extends React.Component {
@@ -27,27 +28,32 @@ class Header extends React.Component {
     return (
       <Navbar fixedTop inverse staticTop>
           <Navbar.Header>
-              <Navbar.Brand>
-              <a href="/">
-                  <img src={logo} className="img-resposive" />
-              </a>                  
-            </Navbar.Brand>
-             <Navbar.Toggle data-toggle="collapse" data-target="#bs-example-navbar-collapse-1"/>
+              <NavbarBrand>
+                <img src={logo} className="img-resposive" />
+            </NavbarBrand>
+            <Navbar.Toggle/>
           </Navbar.Header>
-          <Navbar.Collapse>
-          <Nav>
-            <NavItem eventKey={1} href="/"> Home </NavItem>
-            <NavItem eventKey={2} href="about">About Us</NavItem>
-            <NavDropdown eventKey={6} title="Services" id="basic-nav-dropdown">
-                <MenuItem eventKey={6.1} href="bus-booking">Bus Ticket</MenuItem>
-                <MenuItem eventKey={6.2} href="cab-booking">Cab Booking</MenuItem>
-                <MenuItem eventKey={6.3} href="hotel-booking">Hotal Booking</MenuItem>
-            </NavDropdown>
-            <NavItem eventKey={3} href="privacy">Privacy</NavItem>
-            <NavItem eventKey={4} href="/terms"> Term & Condition </NavItem>
-            <NavItem eventKey={5} href="/faq"> FAQs </NavItem>  
-            <NavItem eventKey={7} href="contact-us"> Contact US </NavItem>  
+          <Navbar.Collapse className="navbar-collapse">
+            <Nav>
+              <NavItem eventKey={1} href="/"> Home </NavItem>
+              <NavItem eventKey={2} href="/about">About Us</NavItem>
+              <NavDropdown eventKey={6} title="Services" id="basic-nav-dropdown">
+                  <MenuItem eventKey={6.1} href="/bus-booking">Bus Ticket</MenuItem>
+                  <MenuItem eventKey={6.2} href="/cab-booking">Cab Booking</MenuItem>
+                  <MenuItem eventKey={6.3} href="/hotel-booking">Hotal Booking</MenuItem>
+              </NavDropdown>
+              <NavItem eventKey={3} href="/privacy">Privacy</NavItem>
+              <NavItem eventKey={5} href="/faq"> FAQs </NavItem>  
+              <NavItem eventKey={7} href="/contact-us"> Contact US </NavItem>  
           </Nav>
+          { !loginData ?
+           <Nav pullRight>
+              <NavItem href="/login"> Login </NavItem>
+              <NavItem href="/signup"> SignUp </NavItem>
+          </Nav> :
+          <Nav pullRight>
+              <NavItem onClick={this.handleLogOut}> Logout </NavItem>
+          </Nav>}
        </Navbar.Collapse>
        </Navbar>
     );
