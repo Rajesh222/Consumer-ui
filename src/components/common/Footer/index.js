149e140b771b40918077d47fb2228f2ecbecab88
Satyam Kumar <satyamk328@gmail.com>
2019-01-27 21:59:09 +0530

    added

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import "./index.scss";
 import config from '../../../config';
-import { Grid, Nav, NavItem } from 'react-bootstrap';
+import { Grid, Row, Col, Nav, NavItem } from 'react-bootstrap';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
 
@@ -15,14 +16,14 @@ class Footer extends React.Component {
   
    render(){ return (
     <footer>
-       <div className="container">
+       <Grid>
           <div className="back-to-top">
                <a href="javascript:void(0)">Back to Top <i className="icon-more-arow ic-box-top"></i></a>
           </div>
-      </div>
-       <div className="container">
-          <div className="row">
-            <div className="col-xs-12 col-sm-4 col-md-4">
+      </Grid>
+       <Grid>
+          <Row>
+            <Col xs={12} sm={4} md={4}>
                  <h4>Get in Touch</h4>
                  <p>Tell us what you think</p>
                  <p>#414, 3rd Floor, 4th Block,17th Main, 100 Feet Road Delhi 110096</p>
@@ -30,30 +31,30 @@ class Footer extends React.Component {
                  <div>
                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62212.873572167366!2d77.61016490000003!3d12.9523506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae145e92832dbd%3A0x46c910f3a5135a4e!2sEzone!5e0!3m2!1sen!2sin!4v1493967009571" width={200} height={200} frameBorder={1} allowFullScreen={true}></iframe>
                  </div>
-            </div>
+            </Col>
             <div className="clearfix visible-xs"></div>
-            <div className="col-xs-12 col-sm-4 col-md-4">
+            <Col xs={12} sm={4} md={4}>
                 <h4>Quick Links</h4>
-               <ul className="list-unstyled quick-links">
-                  <li><a href="/about">About</a></li>
-                  <li><a href="/career">Careers</a></li>
-                  <li><a href="/offers">Offers</a></li>
-                  <li><a href="/privacy">Privacy Policy</a></li>
-                  <li><a href="/terms">Terms & Conditions</a></li>
-                </ul>
-            </div>
+               <Nav className="list-unstyled">
+                  <NavItem href="/about">About</NavItem>
+                  <NavItem href="/career">Careers</NavItem>
+                  <NavItem href="/offers">Offers</NavItem>
+                  <NavItem href="/privacy">Privacy Policy</NavItem>
+                  <NavItem href="/terms">Terms & Conditions</NavItem>
+                </Nav>
+            </Col>
             <div className="clearfix visible-xs"></div>
-            <div className="col-xs-12 col-sm-4 col-md-4">
+            <Col xs={12} sm={4} md={4}>
                  <h4>Services</h4>
-                 <ul className="list-unstyled quick-links">
-                  <li><a href="/bus-booking">Bus Ticket</a></li>
-                  <li><a href="/cab-ticket">Cab Booking</a></li>
-                  <li><a href="/hotel-booking">Hotals Booking</a></li>
-                </ul>
-            </div>
-        </div>
-        <div className="row">
-          <div className="col-xs-12 col-sm-12 col-md-12 mt-2 mt-sm-5">
+               <Nav className="list-unstyled">
+                  <NavItem href="/bus-booking">Bus Ticket</NavItem>
+                  <NavItem href="/cab-ticket">Cab Booking</NavItem>
+                  <NavItem href="/hotel-booking">Hotals Booking</NavItem>
+                </Nav>                
+            </Col>
+        </Row>
+        <Row>
+          <Col xs={12} sm={12} md={12}  className="mt-2 mt-sm-5">
               <ul className="list-unstyled list-inline social text-center">
                 <li className="list-inline-item"><a href="javascript:void();"><FontAwesomeIcon icon="fa fa-facebook"/><i className="fa fa-facebook"></i></a></li>
                 <li className="list-inline-item"><a href="javascript:void();"><i className="fa fa-twitter"></i></a></li>
@@ -61,9 +62,9 @@ class Footer extends React.Component {
                 <li className="list-inline-item"><a href="javascript:void();"><i className="fa fa-google-plus"></i></a></li>
                 <li className="list-inline-item"><a href="javascript:void();" target="_blank"><i className="fa fa-envelope"></i></a></li>
               </ul>
-          </div>
-      </div>       
-      </div>
+          </Col>
+      </Row>       
+      </Grid>
       <div className="sub-footer">
          <div className="container">
             <div className="row">
