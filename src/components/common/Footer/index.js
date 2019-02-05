0a950ad72bd2ad9c2a5efceab30d236793460065
Satyam Kumar <satyam.kumar@agreeya.net>
2019-02-05 17:47:12 +0530

    sadf

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -14,21 +14,19 @@ class Footer extends React.Component {
     <footer className="myfooter">
        <Grid>
           <Row>
-          <Col xs={12} sm={4} md={4}>
+          <Col xs={12} sm={4} md={3}>
                  <h4>Company</h4>
                  <Nav className="list-unstyled">
                    <NavItem href="/privacypolicy">Privacy Policy</NavItem>
                    <NavItem href="/termscondition">Terms & Conditions</NavItem>
                    <NavItem href="/user-agreement">User Agreement</NavItem>
                    <NavItem href="/via-gst-details">Via GST Details</NavItem>
-                   <NavItem href="/about">About Us</NavItem>
-                   <NavItem href="/faq">FAQ</NavItem>
-                   <NavItem href="/careers">Career</NavItem>
-                   <NavItem href="/contact-us">Contact Us</NavItem>        
+                   <NavItem href="/about">About Us</NavItem>                   
+                   <NavItem href="/careers">Career</NavItem>                        
                 </Nav>
             </Col>
             <div className="clearfix visible-xs"></div>
-            <Col xs={12} sm={4} md={4}>
+            <Col xs={12} sm={4} md={3}>
                  <h4>Product Services</h4>
                <Nav className="list-unstyled">
                   <NavItem href="/bus-booking">Bus Ticket</NavItem>
@@ -38,9 +36,17 @@ class Footer extends React.Component {
                 </Nav>                
             </Col>
             <div className="clearfix visible-xs"></div>
-            <Col xs={12} sm={4} md={4}>
+            <Col xs={12} sm={4} md={3}>
+                 <h4>Support</h4>
+                 <Nav className="list-unstyled">
+                    <NavItem href="/contact-us">Contact Us</NavItem>  
+                    <NavItem href="/faq">FAQ</NavItem>
+                 </Nav>                
+            </Col>
+            <div className="clearfix visible-xs"></div>
+            <Col xs={12} sm={4} md={3}>
                 <div>
-                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62212.873572167366!2d77.61016490000003!3d12.9523506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae145e92832dbd%3A0x46c910f3a5135a4e!2sEzone!5e0!3m2!1sen!2sin!4v1493967009571" width={200} height={200} frameBorder={1} allowFullScreen={true}></iframe>
+                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62212.873572167366!2d77.61016490000003!3d12.9523506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae145e92832dbd%3A0x46c910f3a5135a4e!2sEzone!5e0!3m2!1sen!2sin!4v1493967009571" width={200} height={150} frameBorder={1} allowFullScreen={true}></iframe>
                  </div>
             </Col>            
         </Row>              
