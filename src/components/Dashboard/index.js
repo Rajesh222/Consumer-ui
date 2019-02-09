3e4f9858eaf711c5447727cd0f7c69fc427a4a7c
Satyam Kumar <satyam.kumar@agreeya.net>
2019-02-09 21:50:35 +0530

    sdf

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CarouselPage from '../CarouselPage';
 import "./index.scss";
-import { Grid, Row,Col,Label, Input,Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import { Grid, Row,Col,Label, Input,Button, FormGroup, FormControl, Table } from "react-bootstrap";
 import Loader from '../common/Loader';
 
 
@@ -9,14 +9,75 @@ export default class RootPage extends Component {
 
     render() {
         return (
-        <div className="container">
-        <Loader type={"balls"} color={"red"}/>
-            <div className="row">
-                <div className="col-md-12">
+        <Grid>
+           <Row>
+                <Col md={12}>
                     <CarouselPage />
-                </div>
-            </div>
-            <div className="row">
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12} >
+                  <h3>Top Bus Routes</h3>
+                </Col>
+                <Col xs={12}>
+                  <Table striped bordered hover table table-condensed table-responsive>
+                    <thead>
+                        <tr>
+                            <th>Route</th>
+                            <th>Price</th>
+                            <th>Total Operator</th>
+                            <th>Bus</th>
+                            <th>Durection</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Delhi to Gaya</td>
+                            <td>900</td>
+                            <td>30</td>
+                            <td>30</td>
+                            <td>1300 KM</td>
+                        </tr>
+                        <tr>
+                        <td>Delhi to Patna</td>
+                            <td>900</td>
+                            <td>30</td>
+                            <td>30</td>
+                            <td>1400 KM</td>
+                        </tr>
+                        <tr>
+                            <td>Delhi to Allahabad</td>
+                            <td>200</td>
+                            <td>50</td>
+                            <td>95</td>
+                            <td>900 KM</td>
+                        </tr>
+                        <tr>
+                            <td>Indore To Bhopal</td>
+                            <td>200</td>
+                            <td>50</td>
+                            <td>9</td>
+                            <td>195 KM</td>
+                        </tr>
+                        <tr>
+                            <td>Pune To Indore</td>
+                            <td>600</td>
+                            <td>12</td>
+                            <td>5</td>
+                            <td>593 KM</td>
+                        </tr> 
+                        <tr>
+                            <td>Bangalore to Hyderabad</td>
+                            <td>500</td>
+                            <td>2</td>
+                            <td>4</td>
+                            <td>900</td>
+                        </tr>                       
+                    </tbody>
+                  </Table>
+                </Col>
+            </Row>
+            <Row>
               <div className="testimonials">
                 <div className="q-left">“</div>
                     <div className="q-mid">
@@ -32,8 +93,8 @@ export default class RootPage extends Component {
                   
                 <div className="q-right"> ”</div>
                </div>
-           </div>     
-        </div>
+           </Row>   
+        </Grid>
         );
     }
 }
