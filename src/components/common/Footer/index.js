5a737158a7b5932611be11c9d0bfe129e0f21e0b
Satyam Kumar <satyam.kumar@agreeya.net>
2019-02-10 23:35:42 +0530

    sdf

--- src/components/common/Footer/index.js
+++ src/components/common/Footer/index.js
@@ -14,8 +14,8 @@ class Footer extends React.Component {
     <footer className="myfooter">
        <Grid>
           <Row>
-          <Col xs={12} sm={4} md={3}>
-                 <h4>Company</h4>
+          <Col xs={12} sm={3} md={3}>
+                 <h4 className="heading">Discover</h4>
                  <Nav className="list-unstyled">
                    <NavItem href="/about">About Us</NavItem> 
                    <NavItem href="/careers">Career</NavItem>
@@ -24,9 +24,8 @@ class Footer extends React.Component {
                    <NavItem href="/via-gst-details">Via GST Details</NavItem>                                      
                 </Nav>
             </Col>
-            <div className="clearfix visible-xs"></div>
-            <Col xs={12} sm={4} md={3}>
-                 <h4>Product Services</h4>
+            <Col xs={12} sm={3} md={3}>
+                 <h4 className="heading">Product Services</h4>
                <Nav className="list-unstyled">
                   <NavItem href="/bus-booking">Bus Ticket</NavItem>
                   <NavItem href="/cab-ticket">Cab Booking</NavItem>
@@ -36,7 +35,7 @@ class Footer extends React.Component {
             </Col>
             <div className="clearfix visible-xs"></div>
             <Col xs={12} sm={4} md={3}>
-                 <h4>Help & Support</h4>
+                 <h4 className="heading">Help & Support</h4>
                  <Nav className="list-unstyled">
                     <NavItem href="/">Home</NavItem>                    
                     <NavItem href="/paymentoption">Payment Option</NavItem>  
@@ -45,19 +44,14 @@ class Footer extends React.Component {
                  </Nav>                
             </Col>
             <Col xs={12} sm={4} md={3}>
-                 <h4>Policies</h4>
+                 <h4 className="heading">Policies</h4>
                  <Nav className="list-unstyled">
                    <NavItem href="/privacypolicy">Privacy Policy</NavItem>
                    <NavItem href="/termscondition">Terms & Conditions</NavItem>
                    <NavItem href="/faq">Refund Policy</NavItem>                   
                  </Nav>                
             </Col>
-           {/* <div className="clearfix visible-xs"></div>
-            <Col xs={12} sm={4} md={3}>
-                <div>
-                 <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d62212.873572167366!2d77.61016490000003!3d12.9523506!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae145e92832dbd%3A0x46c910f3a5135a4e!2sEzone!5e0!3m2!1sen!2sin!4v1493967009571" width={200} height={150} frameBorder={1} allowFullScreen={true}></iframe>
-                 </div>
-           </Col>   */  }       
+               
         </Row>              
       </Grid>
       <section className="copyright-box">
