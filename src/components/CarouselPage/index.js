bb3595dc6303f2c92b25cdd1a72df60ce139cae7
Satyam Kumar <satyam.kumar@agreeya.net>
2019-02-17 18:03:30 +0530

    adsf

--- src/components/CarouselPage/index.js
+++ src/components/CarouselPage/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./index.scss";
 import { Carousel } from 'react-bootstrap';
 import Two from '../../img/carouser/2.jpg';
 import Three from '../../img/carouser/3.png';
@@ -28,21 +29,21 @@ export default class CarouselPage extends React.Component {
       return (
         <Carousel activeIndex={index} direction={direction} onSelect={this.handleSelect} data-ride="carousel">
           <Carousel.Item>
-            <img className="img-fluid" src={Two}/>
+            <img className="img-responsive" src={Two} style={{height:"300px",width:"100%"}}/>
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Online Bus Tikcet Booking</h3>
+              <p>Digital Bihar is fastest growing application</p>
           </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-             <img className="img-fluid" src={Three}/>
+             <img className="img-responsive" src={Three} style={{height:"300px",width:"100%"}}/>
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3>Travel without worry</h3>
+              <p>Insure every journey with Digital Bihar. Get alternate bus arrangements in an unlikely event of bus cancellation else get double of your bus booking amount refunded.</p>
           </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
-             <img src={Four} className="img-fluid"/>
+             <img src={Four} className="img-responsive" style={{height:"300px",width:"100%"}}/>
             <Carousel.Caption>
               <h3>First slide label</h3>
               <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
